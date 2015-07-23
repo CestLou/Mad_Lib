1c5f309f3cddec1dff832c161a89146146720a27
ddebellis88 <ddebellis88@aol.com>
2015-07-23 16:02:54 +0000

    gets'in all forms are working

--- application_controller.rb
+++ application_controller.rb
@@ -1,4 +1,7 @@
 require 'bundler'
+require_relative 'models/funny.rb'
+require_relative 'models/adventure.rb'
+require_relative 'models/haunted.rb'
 Bundler.require
 
 class ApplicationController < Sinatra::Base
@@ -9,7 +12,17 @@ class ApplicationController < Sinatra::Base
   
   get '/funny_form' do
     @pos=Funny.new.pos
-    erb :inser_word_funny
+    erb :insert_words_funny
+  end
+  
+  get '/adventure_form' do
+    @pos=Adventure.new.pos
+    erb :insert_words_adventure
+  end
+  
+  get '/hunted_form' do
+    @pos=Hunted.new.pos
+    erb :insert_words_hunted
   end
 
 end
