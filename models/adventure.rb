b3b72318d5e403a95b4c23954c55406d4d692d20
ddebellis88 <ddebellis88@aol.com>
2015-07-23 17:18:20 +0000

    getting post to simi work with the stories

--- models/adventure.rb
+++ models/adventure.rb
@@ -1,5 +1,5 @@
 class Adventure
-  def initialize
+  def initialize (pos)
   @pos=["proper_noun", "adjective", "noun", "exclamation", "adjective", "proper_noun", "adjective", "noun", "noun", "adjective", "plural_noun","noun", "adverb", "adjective","adjective", "adverb", "noun"]
   end
   attr_accessor :pos
--- models/haunted.rb
+++ models/haunted.rb
@@ -1,5 +1,5 @@
 class Haunted
-  def initialize
+  def initialize (pos)
     @pos=["proper_noun", "noun", "adjective", "animal", "tool", "verb", "verb_ing", "verb", "noun", "animal_plural", "noun"]
     end
   attr_accessor :pos
