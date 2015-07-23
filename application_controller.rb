dae2742c14282d159aabb75615abb35bfe398236
ddebellis88 <ddebellis88@aol.com>
2015-07-23 15:36:14 +0000

    added more insertfiles along with some code in funny.rb

--- application_controller.rb
+++ application_controller.rb
@@ -6,5 +6,10 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  get '/funny_form' do
+    @pos=Funny.new.pos
+    erb :inser_word_funny
+  end
 
 end
--- models/funny.rb
+++ models/funny.rb
@@ -2,5 +2,6 @@ class Funny
   def initilize
     @pos=["noun", "adj", "adj", "noun", "noun", "verb", "adj", "verb", "noun", "noun", "noun", "noun", "noun", "noun", "adj", "verb", "verb", "verb", "noun", "adj", "noun", "verb", "noun", "adj", "verb"]
   end
+  attr_accessor :pos
 end
 
