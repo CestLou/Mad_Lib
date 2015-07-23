939a1979f266fba102108a2a6b4151286b14b9eb
Margo Schoenberg <mschoenberg@baldwinschool.org>
2015-07-23 15:12:31 +0000

    make funny story

---
+++ models/adventure.rb
@@ -0,0 +1,10 @@
+class Adventure
+  def initialize
+  @parts_of_speech=["proper_noun", "adjective", "noun", "exclamation", "adjective", "proper_noun", "adjective", "noun", "noun", "adjective", "plural_noun","noun", "adverb", "adjective","adjective", "adverb", "noun"]
+  end
+  attr_accessor :parts_of_speech
+  end
+
+
+
+
---
+++ models/haunted.rb
@@ -0,0 +1,10 @@
+class Haunted
+  def initialize
+    @parts_of_speech=["proper_noun", "noun", "adjective", "animal", "tool", "verb", "verb_ing", "verb", "noun", "animal_plural", "noun"]
+    end
+  attr_accessor :parts_of_speech
+
+
+
+  
+end
