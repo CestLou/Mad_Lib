5c37fde841eee1379c3a47513e46a3606f0fb35e
ddebellis88 <ddebellis88@aol.com>
2015-07-24 14:35:15 +0000

    Got all the stories to work with all the inputs

--- models/adventure.rb
+++ models/adventure.rb
@@ -1,6 +1,6 @@
 class Adventure
   def initialize
-  @pos=["proper_noun", "adjective", "noun", "exclamation", "adjective", "proper_noun", "adjective", "noun", "noun", "adjective", "plural_noun","noun", "adverb", "adjective","adjective", "adverb", "noun"]
+  @pos=["proper_noun", "adjective", "noun", "exclamation", "adjective", "proper_noun", "adjective", "noun", "noun", "adjective", "plural_noun", "noun", "adverb", "adjective","adjective", "adverb", "noun"]
   end
   attr_accessor :pos
   end
